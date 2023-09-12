1597c5acd31b0b8d9c1b7fde327facf95c992b6d
thsnow <144845196+thsnow@users.noreply.github.com>
2023-09-12 15:06:33 -0500

    Update streamlit_app.py
    Change header/text for second and third through fifth imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.title ("Breakfast Menu")
+streamlit.header ("Breakfast Menu")
 
-streamlit.title("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
 
-streamlit.title ("Kale, Spinach and Rocket Smoothie")
+streamlit.text ("Kale, Spinach and Rocket Smoothie")
 
-streamlit.title ("Hard-Boiled Free-Range Egg")
+streamlit.text ("Hard-Boiled Free-Range Egg")
